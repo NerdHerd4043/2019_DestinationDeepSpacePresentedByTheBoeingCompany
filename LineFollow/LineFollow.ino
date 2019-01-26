9d259a9548263482dbfab17186efb66f4db1f34f
Ethan Hansen <1ethanhansen@gmail.com>
2019-01-26 14:13:31 -0800

    Line following on Arduino

---
+++ LineFollow/LineFollow.ino
@@ -0,0 +1,44 @@
+const int rSensor = 5;
+const int lSensor = 4;
+
+const int startPin = 3;
+const int leftOutPin = 5;
+const int rightOutPin = 6;
+
+int leftReading;
+int rightReading;
+
+bool leftTriggered = false;
+bool rightTriggered = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rightReading = analogRead(rSensor);
+  leftReading = analogRead(lSensor);
+
+//  Serial.print("R:");
+//  Serial.print(rightReading);
+//  Serial.print(" L:");
+//  Serial.println(leftReading);
+
+  if (leftTriggered && rightTriggered) {
+    if (rightReading > 600) {
+      Serial.print("R:");
+      Serial.println(rightReading);
+    } else if (leftReading > 600) {
+      Serial.print("L:");
+      Serial.println(leftReading);
+    }
+  } else if (rightReading > 600) {
+    rightTriggered = true;
+    Serial.println("Right Triggered");
+  } else if (leftReading > 600) {
+    leftTriggered = true;
+    Serial.println("Left Triggered");
+  }
+}
