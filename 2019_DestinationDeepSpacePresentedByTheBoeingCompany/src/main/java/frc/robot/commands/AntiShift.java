a681ac5fa829c96f07e0cd64256cbbbbe146d844
Zynh0722 <Zynh0722@gmail.com>
2019-02-12 19:24:11 -0800

    ExitingShifters

--- 2019_DestinationDeepSpacePresentedByTheBoeingCompany/src/main/java/frc/robot/commands/AntiShift.java
+++ 2019_DestinationDeepSpacePresentedByTheBoeingCompany/src/main/java/frc/robot/commands/AntiShift.java
@@ -31,9 +31,9 @@ public class AntiShift extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
-
+  
   // Called once after isFinished returns true
   @Override
   protected void end() {
